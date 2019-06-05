7c15a29e6766cd8d5d002e87bdf6a39dbbd34da9
narupi <narupi1999@gmail.com>
2019-06-05 22:27:48 +0900

    add abc125-b

---
+++ atcoder/abc125/b.py
@@ -0,0 +1,10 @@
+N = int(input())
+v = list(map(int, input().split()))
+c = list(map(int, input().split()))
+ans = 0
+
+for i in range(N):
+    if v[i] > c[i]:
+        ans += v[i] - c[i]
+
+print(ans)
